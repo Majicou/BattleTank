2ca3d69ee1b8c1ab22a53eaf20aea39639e849a5
Alan Courchene <themajicou@gmail.com>
2018-07-30 21:24:05 -0400

    Cleanup of components that do not need to Tick

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -2,7 +2,7 @@
 
 #include "TankBarrel.h"
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Barrel::Elevate() called at speed %f"), DegreesPerSecond);
+	UE_LOG(LogTemp, Warning, TEXT("Barrel::Elevate() called at speed %f"), RelativeSpeed);
 }
