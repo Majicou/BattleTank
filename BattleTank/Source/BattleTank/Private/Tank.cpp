af5230a1368889d7135481796e4f74dd1d9b2526
Alan Courchene <themajicou@gmail.com>
2018-07-31 17:39:39 -0400

    Created fire input action and projectile class

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -29,3 +29,8 @@ void ATank::SetTurretReference(UTankTurret * TurretToSet)
 {
 	TankAimingComponent->SetTurretReference(TurretToSet);
 }
+
+void ATank::Fire()
+{
+	UE_LOG(LogTemp, Warning, TEXT("%f: Tank fires"), GetWorld()->GetTimeSeconds());
+}
